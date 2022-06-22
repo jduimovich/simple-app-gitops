056ecc25e50f36a3e550cb4cac5bfedfcf52dbc9
John Duimovich <jduimovich@gmail.com>
2022-06-22 12:47:37 -0400

    gitops workflow

---
+++ setsecret.cmd
@@ -0,0 +1,11 @@
+
+@echo off  
+  
+IF "%MY_QUAY_PW%"=="" (GOTO :missing_reg_token) 
+gh secret set REGISTRY_PASSWORD -b %MY_QUAY_PW%
+goto :end
+
+:missing_reg_token
+echo missing registry token in MY_DOCKER_PW
+
+:end
---
+++ setsecret.sh
@@ -0,0 +1,8 @@
+
+if [ -z $MY_QUAY_PW ]
+then
+echo  $0: No MY_QUAY_PW set, please set MY_QUAY_PW to your registry password.
+exit
+else 
+gh secret set REGISTRY_PASSWORD -b $MY_QUAY_PW
+fi
